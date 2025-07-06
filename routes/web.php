566eb1f66ca2cb090db88b5cbf4617180ab04dfe
shscar <ahmadghozali87621@gmail.com>
2025-07-06 10:58:42 +0700

    FE-landing-page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
+    return Inertia::render('landing/home');
+})->name('home');
+
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
